1e7e37d0a6788987309d1438ebe82b99eacbcb3f
Alex <alex.tran1502@gmail.com>
2022-09-27 12:08:39 -0500

    Optimized function

--- src/immich/directory_walker.rs
+++ src/immich/directory_walker.rs
@@ -1,27 +1,21 @@
-use std::{char::ToLowercase, ffi::OsStr, path::Path};
+use std::ffi::OsStr;
 
 use walkdir::WalkDir;
 
-pub async fn dir_walk(path: &str) -> Vec<String> {
+pub fn dir_walk(path: &str) -> Vec<String> {
     let valid_paths: Vec<String> = WalkDir::new(path)
         .into_iter()
-        .filter_map(|e| e.ok())
-        .filter(|e| e.file_type().is_file())
-        .map(|e| {
-            match e
-                .path()
-                .extension()
-                .and_then(OsStr::to_str)
-                .unwrap_or("")
-                .to_lowercase()
-                .as_str()
-            {
-                "png" | "jpg" | "jpeg" | "webp" => e.path().display().to_string(),
-                _ => "".to_string(),
-            }
+        .filter_map(|e| match e.ok().filter(|e| e.file_type().is_file()) {
+            Some(e) => match e.path().extension().and_then(OsStr::to_str) {
+                Some(ext) => match ext.to_lowercase().as_str() {
+                    "png" | "jpg" | "jpeg" | "webp" => Some(e.path().display().to_string()),
+                    _ => None,
+                },
+                None => None,
+            },
+            None => None,
         })
-        .filter(|file_path| !file_path.is_empty())
         .collect();
-    println!("valid path {:?}", valid_paths.len());
+
     valid_paths
 }
