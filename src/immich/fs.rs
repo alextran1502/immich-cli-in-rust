ba255316d6afc614179ecb5372632b5fd1bcb8db
Alex <alex.tran1502@gmail.com>
2022-10-09 00:56:45 -0500

    get file metadata

--- src/immich/fs.rs
+++ src/immich/fs.rs
@@ -1,8 +1,9 @@
-use std::{ffi::OsStr, path::Path, str::FromStr};
-
+use chrono::prelude::{DateTime, Utc};
+use itertools::Itertools;
+use std::{ffi::OsStr, path::Path, str::FromStr, time::SystemTime};
 use walkdir::WalkDir;
 
-use crate::FileFilter;
+use crate::{immich::models::UploadAsset, FileFilter};
 use colored::*;
 
 use super::models::DeviceAsset;
@@ -63,36 +64,53 @@ pub fn dir_walk(path: &str, filter: &FileFilter) -> Vec<String> {
     valid_paths
 }
 
-pub fn get_file_metadata(device_asset: &Vec<&DeviceAsset>) {
+pub fn get_file_metadata(device_asset: &Vec<&DeviceAsset>) -> Vec<UploadAsset> {
     println!("[5] {} {}", "Getting file metadata", "...");
-    device_asset.iter().for_each(|asset| {
-        let path = Path::new(&asset.path);
-        let file_name = path.file_name().unwrap().to_str().unwrap();
-        let file_size = path.metadata().unwrap().len();
-        let fild_id = format!("{}-{}", file_name, file_size);
-        let file_type = match path
-            .extension()
-            .unwrap()
-            .to_str()
-            .unwrap()
-            .to_lowercase()
-            .parse::<FileType>()
-            .unwrap()
-        {
-            FileType::Image => "IMAGE",
-            FileType::Video => "VIDEO",
-            FileType::Other => {
-                println!(
-                    "[{}] {} {}",
-                    "✗".red(),
-                    "Unsupported file type",
-                    path.display().to_string().red()
-                );
-
-                "OTHER"
+    device_asset
+        .iter()
+        .map(|asset| {
+            let path = Path::new(&asset.path);
+            let file_name = path.file_name().unwrap().to_str().unwrap();
+            let file_size = path.metadata().unwrap().len();
+            let file_id = format!("{}-{}", file_name, file_size);
+            let file_type = match path
+                .extension()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_lowercase()
+                .parse::<FileType>()
+                .unwrap()
+            {
+                FileType::Image => "IMAGE",
+                FileType::Video => "VIDEO",
+                FileType::Other => "OTHER",
             }
-        };
+            .to_string();
 
-        // println!("{:?}", file_type);
-    });
+            let created_at = match path.metadata().unwrap().created() {
+                Ok(created_time) => iso8601(&created_time),
+                Err(_) => iso8601(&SystemTime::now()),
+            };
+
+            let modified_at = match path.metadata().unwrap().modified() {
+                Ok(modified_time) => iso8601(&modified_time),
+                Err(_) => iso8601(&SystemTime::now()),
+            };
+
+            UploadAsset {
+                id: file_id,
+                path: asset.path.clone(),
+                asset_type: file_type,
+                created_at,
+                modified_at,
+                file_extension: path.extension().unwrap().to_str().unwrap().to_string(),
+            }
+        })
+        .collect_vec()
+}
+
+fn iso8601(st: &std::time::SystemTime) -> String {
+    let dt: DateTime<Utc> = st.clone().into();
+    format!("{}", dt.format("%+"))
 }
--- src/immich/models.rs
+++ src/immich/models.rs
@@ -3,3 +3,13 @@ pub struct DeviceAsset {
     pub id: String,
     pub path: String,
 }
+
+#[derive(Debug, PartialEq)]
+pub struct UploadAsset {
+    pub id: String,
+    pub path: String,
+    pub asset_type: String,
+    pub created_at: String,
+    pub modified_at: String,
+    pub file_extension: String,
+}
